07a44d83a94b2f0fadf84c75e3a55e9ae25bb42b
Lyudmila <milo4kamoya@gmail.com>
2022-05-12 10:36:30 -0700

    Data added

---
+++ src/data.js
@@ -0,0 +1,121 @@
+const data = [
+  {
+    id: 'B1',
+    structureType: 'building',
+    name: 'general hospital',
+    xCoordinate: 1300,
+    yCoordinate: 560
+  },
+  {
+    id: 'B2',
+    structureType: 'building',
+    name: 'firehall',
+    xCoordinate: -350,
+    yCoordinate: 1100
+  },
+  {
+    id: 'B3',
+    structureType: 'building',
+    name: 'university',
+    xCoordinate: -890,
+    yCoordinate: -1300
+  },
+  {
+    id: 'B4',
+    structureType: 'building',
+    name: 'central library',
+    xCoordinate: 470,
+    yCoordinate: 1010
+  },
+  {
+    id: 'B5',
+    structureType: 'building',
+    name: 'secondary school',
+    xCoordinate: 1550,
+    yCoordinate: 850
+  },
+  {
+    id: 'B6',
+    structureType: 'building',
+    name: 'primary school',
+    xCoordinate: 1450,
+    yCoordinate: 950
+  },
+  {
+    id: 'B1',
+    structureType: 'building',
+    name: 'city hall',
+    xCoordinate: -600,
+    yCoordinate: 200
+  },
+  {
+    id: 'C1',
+    structureType: 'cell tower',
+    name: 'Bell NW',
+    xCoordinate: 2200,
+    yCoordinate: 1720
+  },
+  {
+    id: 'C2',
+    structureType: 'cell tower',
+    name: 'Bell NE',
+    xCoordinate: -1090,
+    yCoordinate: 1400
+  },{
+    id: 'C3',
+    structureType: 'cell tower',
+    name: 'Bell SW',
+    xCoordinate: 760,
+    yCoordinate: -1360
+  },{
+    id: 'C4',
+    structureType: 'cell tower',
+    name: 'Bell SE',
+    xCoordinate: -1410,
+    yCoordinate: -450
+  },{
+    id: 'C5',
+    structureType: 'cell tower',
+    name: 'Rogers NW1',
+    xCoordinate: 2300,
+    yCoordinate: 1710
+  },{
+    id: 'C6',
+    structureType: 'cell tower',
+    name: 'Rogers NW2',
+    xCoordinate: 50,
+    yCoordinate: 2080
+  },{
+    id: 'C7',
+    structureType: 'cell tower',
+    name: 'Rogers SE',
+    xCoordinate: -1410,
+    yCoordinate: -450
+  },{
+    id: 'C8',
+    structureType: 'cell tower',
+    name: 'Freedom SW',
+    xCoordinate: 1100,
+    yCoordinate: -1200
+  },{
+    id: 'C9',
+    structureType: 'cell tower',
+    name: 'Freedom NE',
+    xCoordinate: -1090,
+    yCoordinate: 1400
+  },{
+    id: 'C10',
+    structureType: 'cell tower',
+    name: 'TELUS SE1',
+    xCoordinate: -1740,
+    yCoordinate: -50
+  },{
+    id: 'C11',
+    structureType: 'cell tower',
+    name: 'TELUS SE2',
+    xCoordinate: -2300,
+    yCoordinate: -350
+  },
+]
+
+export default data;
